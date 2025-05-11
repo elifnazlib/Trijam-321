24b4fe747b46720920cc5ba6a5877a9bdc299ff6
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-05-11 16:24:10 +0300

    Radar

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
-public class TirePhysic : MonoBehaviour
+public class Vehicle : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D carRigidbody;
     [SerializeField] private Rigidbody2D frontTireRigidbody;
     [SerializeField] private Rigidbody2D backTireRigidbody;
 
@@ -10,5 +12,14 @@ public class TirePhysic : MonoBehaviour
         // Add torque to the tires based on its velocity
         frontTireRigidbody.AddTorque(frontTireRigidbody.velocity.x * Time.fixedDeltaTime); 
         backTireRigidbody.AddTorque(backTireRigidbody.velocity.x * Time.fixedDeltaTime); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Radar"))
+        {
+            // TODO: Implement the logic to handle the vehicle entering the radar area
+            Debug.Log(carRigidbody.velocity.x * 3.6f);
+        }
     }
 }
