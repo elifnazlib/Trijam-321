52f026c1bbed480cbcf6d39825b1f472883cdce5
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2025-05-12 01:19:33 +0300

    Narrative Scene

---
+++ Assets/Scripts/Level0.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Level0 : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D caRigidbody2D;
+    [SerializeField] private GameObject line1;
+    [SerializeField] private GameObject line2;
+    [SerializeField] private GameObject line3;
+    [SerializeField] private GameObject line4;
+
+    private void Start()
+    {
+        StartCoroutine(CallNextLine());
+    }
+
+    IEnumerator CallNextLine()
+    {
+        yield return new WaitForSeconds(2f);
+        caRigidbody2D.AddForce(new Vector2(17, 0), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(6f);
+        line1.SetActive(false);
+        line2.SetActive(true);
+        yield return new WaitForSeconds(7f);
+        line2.SetActive(false);
+        line3.SetActive(true);
+        yield return new WaitForSeconds(7f);
+        line3.SetActive(false);
+        line4.SetActive(true);
+        yield return new WaitForSeconds(10f);
+        LoadNextScene();
+    }
+    
+    public void LoadNextScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
